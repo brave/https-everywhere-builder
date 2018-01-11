648c76438486c5d68e26923a95e56f1774a39830
Chan Chak Shing <ccspasu@gmail.com>
2018-01-11 14:59:42 +0800

    Work around with HTTPSE breaking ruleset format changes
    - Update preloadHTTPSE.js
    - Update package.json
    - Bump version in package.json to 6.0.0
    - Update scripts/preloadHTTPSE.json
    - Update to HTTPS Everywhere 2018.1.29
    - Changes as per npm  run lint
    - Update scripts/preloadHTTPSE.js
    - Simplify HTTPSE.json schema
    - Update package-lock.json

--- scripts/preloadHTTPSE.js
+++ scripts/preloadHTTPSE.js
@@ -1,12 +1,11 @@
 'use strict'
 const fs = require('fs')
 const http = require('https')
-const parseString = require('xml2js').parseString
 const levelup = require('level')
 const rmDir = require('./util').rmDir
 const exec = require('child_process').exec
 
-const xpiVersion = '5.2.21' // Manually update this to latest version
+const xpiVersion = '2018.1.29' // Manually update this to latest version
 
 const downloadRulesets = (dir, cb) => {
   const downloadURL = `https://www.eff.org/files/https-everywhere-${xpiVersion}-eff.xpi`
@@ -36,81 +35,107 @@ const buildDataFiles = () => {
   // Manually exclude sites that are broken until they are fixed in the next
   // HTTPS Everywhere release.
   const exclusions = {
-    'Nike.com.xml': 'breaks nikeplus.com',
-    'PJ_Media.xml': 'mixed content on https://pjmedia.com/instapundit/',
-    'Slashdot.xml': 'redirect loop on mobile slashdot.org',
-    'Delta.com.xml': 'https://delta.com does not redirect to https://www.delta.com',
-    'Cargo.xml': 'breaks cargocollective.com',
-    'TMZ.com.xml': 'breaks www.tmz.com',
-    'BusinessInsider.xml': 'breaks http://www.businessinsider.com/silicon-valley-100-2016-6?op=0',
-    'Tesco.xml': 'breaks tesco.com due to CSP mismatch',
-    'Vodafone.ie.xml': 'breaks pagination on http://shop.vodafone.ie/shop/phonesAndPlans/phonesAndPlansHome.jsp?subPage=phones&planFilter=onAccount',
-    'IDownloadBlog.xml': 'breaks http://www.idownloadblog.com/',
-    'EBay_static.com.xml': 'breaks suggested product image previews',
-    'Cisco.xml': 'breaks http://www.cisco.com/c/m/en_us/training-events/events-webinars/techwise-tv/listings.html',
-    'GQ.xml': 'mixed content on gq.com',
-    'Where_2_Get_It.xml': 'maps missing on http://us.coopertire.com/Customer-Care/Dealer-Locator.aspx?form=locator_search&addressline=92346',
-    'Thompson_Hotels.com.xml': 'missing stylesheets on http://www.thompsonhotels.com/'
+    'Nike.com (partial)': 'breaks nikeplus.com',
+    'PJ Media': 'mixed content on https://pjmedia.com/instapundit/',
+    'Slashdot.org (partial)': 'redirect loop on mobile slashdot.org',
+    'Delta.com (partial)': 'https://delta.com does not redirect to https://www.delta.com',
+    'Cargo (partial)': 'breaks cargocollective.com',
+    'TMZ.com': 'breaks www.tmz.com',
+    'BusinessInsider.com (partial)': 'breaks http://www.businessinsider.com/silicon-valley-100-2016-6?op=0',
+    'Tesco (partial)': 'breaks tesco.com due to CSP mismatch',
+    'Vodafone.ie (partial)': 'breaks pagination on http://shop.vodafone.ie/shop/phonesAndPlans/phonesAndPlansHome.jsp?subPage=phones&planFilter=onAccount',
+    'iDownloadBlog (partial)': 'breaks http://www.idownloadblog.com/',
+    'Cisco.com (partial)': 'breaks http://www.cisco.com/c/m/en_us/training-events/events-webinars/techwise-tv/listings.html',
+    'GQ.com (partial)': 'mixed content on gq.com',
+    'Where 2 Get It (partial)': 'maps missing on http://us.coopertire.com/Customer-Care/Dealer-Locator.aspx?form=locator_search&addressline=92346',
+    'Thompson Hotels.com (partial)': 'missing stylesheets on http://www.thompsonhotels.com/'
   }
 
-  const rulesets = JSON.parse(fs.readFileSync('./https-everywhere/chrome/content/rulesets.json', 'utf8'))
+  const rulesets = JSON.parse(fs.readFileSync('./https-everywhere/rules/default.rulesets', 'utf8'))
 
-  // Convert XML rules to JSON
-  for (let id in rulesets.rulesetStrings) {
-    let contents = rulesets.rulesetStrings[id]
-    parseString(contents, function (err, result) {
-      if (err) {
-        throw new Error('FATAL: error parsing XML: ' + contents)
+  let jsonOutput = {
+    'rulesetStrings': [],
+    'targets': {}
+  }
+
+  for (const ruleset of rulesets) {
+    if (!ruleset.default_off && !ruleset.platform) {
+      if (ruleset.name in exclusions) {
+        console.log('NOTE: Excluding ruleset:', ruleset.name)
+        continue
       }
-      // Exclude broken rules
-      const ruleset = result.ruleset
-      if (ruleset.$.f in exclusions) {
-        console.log('NOTE: Excluding rule', JSON.stringify(result))
-        ruleset.$.default_off = exclusions[ruleset.$.f]
+
+      for (const target of ruleset.target) {
+        if (!jsonOutput['targets'][target]) {
+          jsonOutput['targets'][target] = []
+        }
+        jsonOutput['targets'][target].push(jsonOutput['rulesetStrings'].length)
       }
-      rulesets.rulesetStrings[id] = result
-    })
+
+      let r = {
+        ruleset: {
+          'name': ruleset.name,
+          'rule': ruleset.rule.map((rule) => {
+            return {
+              'from': rule.from,
+              'to': rule.to
+            }
+          })
+        }
+      }
+
+      if (ruleset.exclusion) {
+        r.exclusion = ruleset.exclusion.map((exclusion) => {
+          return {
+            'pattern': exclusion
+          }
+        })
+      }
+
+      jsonOutput['rulesetStrings'].push(r)
+    }
   }
 
   console.log('Writing httpse.json')
-  fs.writeFileSync('./out/httpse.json', JSON.stringify(rulesets), 'utf8')
+  fs.writeFileSync('./out/httpse.json', JSON.stringify(jsonOutput), 'utf8')
 
   console.log('creating httpse.leveldb')
   rmDir('./out/httpse.leveldb')
 
   const httpseLevelDB = levelup('./out/httpse.leveldb', {compression: false, errorIfExists: true})
 
-  const ruleSets = {}
-  for (let id in rulesets.rulesetStrings) {
-    ruleSets[id] = rulesets.rulesetStrings[id]
-  }
-
   let batch = httpseLevelDB.batch()
-  for (let target in rulesets.targets) {
-    let targetRuleSets = []
-    rulesets.targets[target].forEach((id) => {
-      let ruleset = ruleSets[id]
-      if (!ruleset.ruleset.$.default_off && !ruleset.ruleset.$.platform) {
-        let rule = {
-          r: ruleset.ruleset.rule.map((rule) => {
-            if (rule.$.from === '^http:' && rule.$.to === 'https:') {
-              return { d: 1 }
-            } else {
-              return { f: rule.$.from, t: rule.$.to }
-            }
-          })
-        }
-        if (ruleset.ruleset.exclusion) {
-          rule.e = ruleset.ruleset.exclusion.map((exclusion) => {
-            return { p: exclusion.$.pattern }
-          })
-        }
-        targetRuleSets = targetRuleSets.concat(rule)
+
+  for (const ruleset of rulesets) {
+    if (!ruleset.default_off && !ruleset.platform) {
+      if (ruleset.name in exclusions) {
+        console.log('NOTE: Excluding ruleset:', ruleset.name)
+        continue
       }
-    })
-    let reverseTarget = target.split('.').reverse().join('.')
-    if (targetRuleSets.length > 0) {
-      batch.put(reverseTarget, JSON.stringify(targetRuleSets), {sync: true})
+
+      let targetRuleSets = []
+      let rule = {
+        r: ruleset.rule.map((rule) => {
+          if (rule.from === '^http:' && rule.to === 'https:') {
+            return { d: 1 }
+          } else {
+            return { f: rule.from, t: rule.to }
+          }
+        })
+      }
+      if (ruleset.exclusion) {
+        rule.e = ruleset.exclusion.map((exclusion) => {
+          return { p: exclusion.pattern }
+        })
+      }
+      targetRuleSets = targetRuleSets.concat(rule)
+
+      for (const target of ruleset.target) {
+        const reverseTarget = target.split('.').reverse().join('.')
+        if (targetRuleSets.length > 0) {
+          batch.put(reverseTarget, JSON.stringify(targetRuleSets), {sync: true})
+        }
+      }
     }
   }
 
