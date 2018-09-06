9d986783255651e0dc1167a276c040e777926004
Brian R. Bondy <netzen@gmail.com>
2018-09-06 09:39:33 -0400

    Add digg exception

--- scripts/preloadHTTPSE.js
+++ scripts/preloadHTTPSE.js
@@ -35,6 +35,7 @@ const buildDataFiles = () => {
   // Manually exclude sites that are broken until they are fixed in the next
   // HTTPS Everywhere release.
   const exclusions = {
+    'Digg (partial)': 'breaks digg.com on C70+ with NET::ERR_CERT_SYMANTEC_LEGACY',
     'Nike.com (partial)': 'breaks nikeplus.com',
     'PJ Media': 'mixed content on https://pjmedia.com/instapundit/',
     'Slashdot.org (partial)': 'redirect loop on mobile slashdot.org',
